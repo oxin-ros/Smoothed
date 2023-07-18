50804057a690bfebc7fbd6f54d679d799f19fdde
David B <david.b@smartmachine.nz>
2023-07-19 11:01:22 +1200

    cleanup code & fix return of clear.

--- src/Smoothed.h
+++ src/Smoothed.h
@@ -12,188 +12,203 @@
 
 // A class used to store and calculate the values to be smoothed.
 template <typename T>
-class Smoothed {
-  private:
-    byte smoothMode;
-    uint16_t smoothReadingsFactor = 10; // The smoothing factor. In average mode, this is the number of readings to average. 
-    uint16_t smoothReadingsPosition = 0; // Current position in the array
-    uint16_t smoothReadingsNum = 0; // Number of readings currently being averaged
-    T *smoothReading; // Array of readings
-  public:
-    Smoothed();
-    ~Smoothed(); // Destructor to clean up when class instance killed
-    bool begin (byte smoothMode, uint16_t smoothFactor = 10);
-    bool add (T newReading);
-    T get ();
-    T getLast ();
-    bool clear ();
+class Smoothed
+{
+private:
+  byte smoothMode;
+  uint16_t smoothReadingsFactor = 10;  // The smoothing factor. In average mode, this is the number of readings to average.
+  uint16_t smoothReadingsPosition = 0; // Current position in the array
+  uint16_t smoothReadingsNum = 0;      // Number of readings currently being averaged
+  T *smoothReading;                    // Array of readings
+public:
+  Smoothed();
+  ~Smoothed(); // Destructor to clean up when class instance killed
+  bool begin(byte smoothMode, uint16_t smoothFactor = 10);
+  bool add(T newReading);
+  T get();
+  T getLast();
+  bool clear();
 };
 
 // Constructor
 template <typename T>
-Smoothed<T>::Smoothed () { // Constructor
-  
+Smoothed<T>::Smoothed()
+{ // Constructor
 }
 
 // Destructor
 template <typename T>
-Smoothed<T>::~Smoothed () { // Destructor
+Smoothed<T>::~Smoothed()
+{ // Destructor
   delete[] smoothReading;
 }
 
 // Inintialise the array for storing sensor values
 template <typename T>
-bool Smoothed<T>::begin (byte mode, uint16_t smoothFactor) { 
+bool Smoothed<T>::begin(byte mode, uint16_t smoothFactor)
+{
   smoothMode = mode;
-  smoothReadingsFactor = smoothFactor; 
-  
-  switch (smoothMode) {  
-    case SMOOTHED_AVERAGE : // SMOOTHED_AVERAGE
-      
-      smoothReading = new T[smoothReadingsFactor]; // Create the actual array of the required size
-    
-      // Initialise all the values in the array to zero
-      for (int thisReading = 0; thisReading < smoothReadingsNum; thisReading++) {
-        smoothReading[thisReading] = 0;
-      }
+  smoothReadingsFactor = smoothFactor;
 
-      return true;
-      break;
-      
-    case SMOOTHED_EXPONENTIAL : // SMOOTHED_EXPONENTIAL
+  switch (smoothMode)
+  {
+  case SMOOTHED_AVERAGE: // SMOOTHED_AVERAGE
+  {
+    smoothReading = new T[smoothReadingsFactor]; // Create the actual array of the required size
 
-      smoothReading = new T[2];
-      smoothReading[0] = 0;
-      smoothReading[1] = 0; // Second value in array used for storing last value added
-      
-      return true;  
-      break;
+    // Initialise all the values in the array to zero
+    for (int thisReading = 0; thisReading < smoothReadingsNum; thisReading++)
+    {
+      smoothReading[thisReading] = 0;
+    }
 
-    default : 
-      return false;
-      break;
+    return true;
   }
-  
+  case SMOOTHED_EXPONENTIAL: // SMOOTHED_EXPONENTIAL
+  {
+    smoothReading = new T[2];
+    smoothReading[0] = 0;
+    smoothReading[1] = 0; // Second value in array used for storing last value added
+
+    return true;
+  }
+  default:
+    return false;
+  }
 }
 
 // Add a value to the array
 template <typename T>
-bool Smoothed<T>::add (T newReading) {
-  switch (smoothMode) {    
-    case SMOOTHED_AVERAGE : // SMOOTHED_AVERAGE
-    
-      if(smoothReadingsNum < smoothReadingsFactor) { smoothReadingsNum++; } // Keep record of the number of readings being averaged. This will count up to the arrany saize then stay at that number
-       
-      smoothReading[smoothReadingsPosition] = newReading; // Add the new value
-      
-      if (smoothReadingsPosition == (smoothReadingsFactor - 1)) { // If at the end of the array
-        smoothReadingsPosition = 0; // Increment to the beginning of the array
-      } else {
-        smoothReadingsPosition++; // Increment to next array position position
-      }
+bool Smoothed<T>::add(T newReading)
+{
+  switch (smoothMode)
+  {
+  case SMOOTHED_AVERAGE: // SMOOTHED_AVERAGE
+  {
+    if (smoothReadingsNum < smoothReadingsFactor)
+    {
+      smoothReadingsNum++;
+    } // Keep record of the number of readings being averaged. This will count up to the arrany saize then stay at that number
 
-      return true;
-      break;
+    smoothReading[smoothReadingsPosition] = newReading; // Add the new value
 
-    case SMOOTHED_EXPONENTIAL : // SMOOTHED_EXPONENTIAL
+    if (smoothReadingsPosition == (smoothReadingsFactor - 1))
+    {                             // If at the end of the array
+      smoothReadingsPosition = 0; // Increment to the beginning of the array
+    }
+    else
+    {
+      smoothReadingsPosition++; // Increment to next array position position
+    }
 
-      if( smoothReadingsNum == 0 ) {
-        smoothReadingsNum++;
-        smoothReading[0] = newReading;
-      } else {
-        smoothReading[0] = (T)(((long double)smoothReadingsFactor/100) * newReading + (1 - ((long double)smoothReadingsFactor/100)) * smoothReading[0]);
-      }
+    return true;
+  }
+  case SMOOTHED_EXPONENTIAL: // SMOOTHED_EXPONENTIAL
+  {
+    if (smoothReadingsNum == 0)
+    {
+      smoothReadingsNum++;
+      smoothReading[0] = newReading;
+    }
+    else
+    {
+      smoothReading[0] = (T)(((long double)smoothReadingsFactor / 100) * newReading + (1 - ((long double)smoothReadingsFactor / 100)) * smoothReading[0]);
+    }
 
-      smoothReading[1] = newReading; // Update the last value added
-      
-      return true;
-      break;
-      
-    default : 
-      return false;
-      break;
-  }    
+    smoothReading[1] = newReading; // Update the last value added
+
+    return true;
+  }
+  default:
+    return false;
+  }
 }
 
 // Get the smoothed result
 template <typename T>
-T Smoothed<T>::get () {
-  switch (smoothMode) {
-    case SMOOTHED_AVERAGE : { // SMOOTHED_AVERAGE
-      T runningTotal = 0;
-      // calculating a `SUM(smoothReadings) / smoothReadingsNum` can lead to overflows.
-      T tmpRes = 0;
-      T remainder = 0;
-      for (int x = 0; x < smoothReadingsNum; x++) {
-        tmpRes = smoothReading[x] / smoothReadingsNum;
-        remainder += smoothReading[x] - tmpRes * smoothReadingsNum;
+T Smoothed<T>::get()
+{
+  switch (smoothMode)
+  {
+  case SMOOTHED_AVERAGE:
+  { // SMOOTHED_AVERAGE
+    T runningTotal = 0;
+    // calculating a `SUM(smoothReadings) / smoothReadingsNum` can lead to overflows.
+    T tmpRes = 0;
+    T remainder = 0;
+    for (int x = 0; x < smoothReadingsNum; x++)
+    {
+      tmpRes = smoothReading[x] / smoothReadingsNum;
+      remainder += smoothReading[x] - tmpRes * smoothReadingsNum;
+      runningTotal += tmpRes;
+      if (remainder > smoothReadingsNum)
+      {
+        tmpRes = remainder / smoothReadingsNum;
+        remainder -= tmpRes * smoothReadingsNum;
         runningTotal += tmpRes;
-        if (remainder > smoothReadingsNum) {
-          tmpRes = remainder / smoothReadingsNum;
-          remainder -= tmpRes * smoothReadingsNum;
-          runningTotal += tmpRes;
-        }
       }
-      return runningTotal;
     }
-      break;
-
-    case SMOOTHED_EXPONENTIAL : // SMOOTHED_EXPONENTIAL
-      return smoothReading[0]; 
-      break;
-
-    default : 
-      return false;
-      break;
-  }   
+    return runningTotal;
+  }
+  case SMOOTHED_EXPONENTIAL: // SMOOTHED_EXPONENTIAL
+    return smoothReading[0];
+  default:
+    return false;
+  }
 }
 
 // Gets the last result stored
 template <typename T>
-T Smoothed<T>::getLast () {
-  switch (smoothMode) {  
-    case SMOOTHED_AVERAGE : // SMOOTHED_AVERAGE 
-      // Just return the last reading
-      if (smoothReadingsPosition == 0) {
-        return smoothReading[smoothReadingsFactor-1]; 
-      } else {
-        return smoothReading[smoothReadingsPosition-1];
-      }
-      break;
-
-    case SMOOTHED_EXPONENTIAL : // SMOOTHED_EXPONENTIAL
-      return smoothReading[1]; 
-      break;
-
-    default : 
-      return false;
-      break;
-  }         
+T Smoothed<T>::getLast()
+{
+  switch (smoothMode)
+  {
+  case SMOOTHED_AVERAGE: // SMOOTHED_AVERAGE
+  {
+    // Just return the last reading
+    if (smoothReadingsPosition == 0)
+    {
+      return smoothReading[smoothReadingsFactor - 1];
+    }
+    else
+    {
+      return smoothReading[smoothReadingsPosition - 1];
+    }
+  }
+  case SMOOTHED_EXPONENTIAL: // SMOOTHED_EXPONENTIAL
+    return smoothReading[1];
+  default:
+    return false;
+  }
 }
 
 // Clears all stored values
 template <typename T>
-bool Smoothed<T>::clear () {
-  switch (smoothMode) {    
-    case SMOOTHED_AVERAGE : // SMOOTHED_AVERAGE    
-      // Reset the counters
-      smoothReadingsPosition = 0; 
-      smoothReadingsNum = 0; 
-      
-      // Set all the values in the array to zero.  Not really needed
-      for (int thisReading = 0; thisReading < smoothReadingsNum; thisReading++) {
-        smoothReading[thisReading] = 0;
-      }
-      break;
+bool Smoothed<T>::clear()
+{
+  switch (smoothMode)
+  {
+  case SMOOTHED_AVERAGE: // SMOOTHED_AVERAGE
+  {
+    // Reset the counters
+    smoothReadingsPosition = 0;
+    smoothReadingsNum = 0;
 
-    case SMOOTHED_EXPONENTIAL : // SMOOTHED_EXPONENTIAL
-      smoothReadingsNum = 0;
-	  smoothReading[0] = 0;
-      smoothReading[1] = 0;
-      break;
-
-    default : 
-      return false;
-      break;
-  }         
+    // Set all the values in the array to zero.  Not really needed
+    for (int thisReading = 0; thisReading < smoothReadingsNum; thisReading++)
+    {
+      smoothReading[thisReading] = 0;
+    }
+    return true;
+  }
+  case SMOOTHED_EXPONENTIAL: // SMOOTHED_EXPONENTIAL
+  {
+    smoothReadingsNum = 0;
+    smoothReading[0] = 0;
+    smoothReading[1] = 0;
+    return true;
+  }
+  default:
+    return false;
+  }
 }
